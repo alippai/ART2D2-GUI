481475daeba264f4876923656abc017c5513b5ba
alippai <adam@rigo.sk>
2013-09-02 23:56:42 +0200

    Initial keyboard handling.

--- app.js
+++ app.js
@@ -6,24 +6,6 @@ var WebSocketServer = require('ws').Server
 
 var client = dgram.createSocket("udp4");
 
-var commands = {
-  CONNECT: 0,
-  STOP: 1,
-  FORWARD: 2,
-  BACKWARD: 3,
-  LEFT: 4,
-  RIGHT: 5,
-  FLEFT: 6,
-  FRIGHT: 7,
-  BLEFT: 8,
-  BRIGHT: 9,
-  PULLUP: 10,
-  PUTDOWN: 11,
-  UDPTEST: 12,
-  MEASUREFOR: 13,
-  MEASUREBACK: 14
-};
-
 app.use(express.static(__dirname + '/client'));
 
 var server = http.createServer(app);
@@ -32,35 +14,23 @@ server.listen(8080);
 var wss = new WebSocketServer({server: server});
 
 wss.on('connection', function(ws) {
-  ws.on('message', function (message) {
-    message = JSON.parse(message);
-    if (message.type == 'command') {
-      commandExec(message.data);
-    }
-  });
-});
-
-function commandExec(command) {
-  switch (command) {
-    case 'connect':
-      connect();
-      break;
-  }
-}
-
-function connect() {
-  var msg = new Buffer([commands.CONNECT]);
-  console.log('sendconnect');
+  var msgConnect  = new Buffer([commands.CONNECT]);
   client.send(msg, 0, msg.length, 44100, '10.8.11.91', function(err, bytes) {
     console.log('connect sent');
     client.close();
   });
-}
 
-client.on("message", function (msg, rinfo) {
-  console.log('msg');
-  console.log(msg);
-  console.log('rinfo');
-  console.log(rinfo);
+  ws.on('message', function (message) {
+    client.send(msg, 0, msg.length, 44100, '10.8.11.91', function(err, bytes) {
+      console.log('connect sent');
+      client.close();
+    });
+  });
+
+  client.on("message", function (msg, rinfo) {
+    console.log('msg');
+    console.log(msg);
+    console.log('rinfo');
+    console.log(rinfo);
+  });
 });
-
--- client/script.js
+++ client/script.js
@@ -1,17 +1,118 @@
+var globalState = globalReset();
+var marker = false;
+
+var commandList = {
+  CONNECT: 0,
+  STOP: 1,
+  FORWARD: 2,
+  BACKWARD: 3,
+  LEFT: 4,
+  RIGHT: 5,
+  FLEFT: 6,
+  FRIGHT: 7,
+  BLEFT: 8,
+  BRIGHT: 9,
+  PULLUP: 10,
+  PUTDOWN: 11,
+  UDPTEST: 12,
+  MEASUREFOR: 13,
+  MEASUREBACK: 14
+};
+
 var connection = new WebSocket("ws://localhost:8080/");
-$connect = $('#connect');
 
 connection.onopen = function () {
-  $connect.removeAttr('disabled');
-  $connect.removeClass('disabled');
+  setInterval(send, 750);
+  console.log('connected');
 };
 
 connection.onmessage = function (message) {
-  console.log('message received: ' + message.data);
+  console.log(message);
+  console.log(new Uint8Array(message));
 };
 
-$connect.on('click', function (e) {
-  connection.send(JSON.stringify({ type: 'command', data: 'connect'}));
+$(document).on('keydown', function (e) {
+  switch (e.keyCode) {
+    // `w`
+    case 87:
+      globalState.w = true;
+      if (globalState.a) {
+        setCommand(commandList.FLEFT);
+      } else if (globalState.d) {
+        setCommand(commandList.FRIGHT);
+      } else {
+        setCommand(commandList.FORWARD);
+      }
+      break;
+    // `a`
+    case 65:
+      globalState.a = true;
+      if (globalState.w) {
+        setCommand(commandList.FLEFT);
+      } else if (globalState.s) {
+        setCommand(commandList.BLEFT);
+      } else {
+        setCommand(commandList.LEFT);
+      }
+      break;
+    // `s`
+    case 83:
+      globalState.s = true;
+      if (globalState.a) {
+        setCommand(commandList.BLEFT);
+      } else if (globalState.d) {
+        setCommand(commandList.BRIGHT);
+      } else {
+        setCommand(commandList.BACKWARD);
+      }
+      break;
+    // `d`
+    case 68:
+      globalState.d = true;
+      if (globalState.w) {
+        setCommand(commandList.FRIGHT);
+      } else if (globalState.s) {
+        setCommand(commandList.BRIGHT);
+      } else {
+        setCommand(commandList.RIGHT);
+      }
+      break;
+    // `m`
+    case 77:
+      marker = !marker;
+      setCommand(marker ? commandList.PULLUP : commandList.PUTDOWN);
+      break;
+
+  }
+
   e.preventDefault();
   return false;
 });
+
+$(document).on('keyup', function (e) {
+  globalState = globalReset();
+  setCommand(commandList.STOP);
+
+  e.preventDefault();
+  return false;
+});
+
+function setCommand(code) {
+  globalState.command = new ArrayBuffer(1);
+  var view = new Uint8Array(code);
+  view[0] = code;
+}
+
+function send() {
+  client.send(globalState.command);
+}
+
+function globalReset() {
+  return {
+    command: null,
+    w: false,
+    a: false,
+    s: false,
+    d: false
+  };
+}
