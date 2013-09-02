f2dd6da1d30a44a7ddb5a1701c67d830e61041f8
alippai <adam@rigo.sk>
2013-09-02 22:07:25 +0200

    Add sample css, add sample Connect button.

--- client/script.js
+++ client/script.js
@@ -1,9 +1,17 @@
 var connection = new WebSocket("ws://localhost:8080/");
+$connect = $('#connect');
 
 connection.onopen = function () {
-  connection.send('ping');
+  $connect.removeAttr('disabled');
+  $connect.removeClass('disabled');
 };
 
 connection.onmessage = function (message) {
   console.log('message received: ' + message.data);
 };
+
+$connect.on('click', function (e) {
+  connection.send('connect');
+  e.preventDefault();
+  return false;
+});
